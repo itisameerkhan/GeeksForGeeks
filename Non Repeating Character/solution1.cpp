73d4cfca717ebef51b1376a01a8a313926b8af33
itisameerkhan <itisameerkhan@gmail.com>
2023-11-15 22:28:23 +0530

    15/11/2023 22:28

---
+++ Non Repeating Character/solution1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    char nonrepeatingCharacter(string S)
+    {
+       unordered_map<char, int> map;
+       for(int i=0;i<S.size();i++) map[S[i]]++;
+       for(int i=0;i<S.size();i++) if(map[S[i]] == 1) return S[i];
+       return '$';
+    }
+};
+int main() {
+    string S = "hello";
+    Solution sol;
+    cout<<sol.nonrepeatingCharacter(S);
+}
